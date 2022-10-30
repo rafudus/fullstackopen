b57a4bdaca1e329a00c8c304ba45780eaa204c4c
rafudus <rafudus@gmail.com>
2022-10-30 21:19:26 +0100

    anecdotes 1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,12 +10,29 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+
+  let votes = new Uint8Array(anecdotes.length);
    
   const [selected, setSelected] = useState(0)
+
+  const handleNext = () => {
+    const min = 0;
+    const max = anecdotes.length-1;
+    const rand =parseInt(min + Math.random() * (max - min), 10);
+    setSelected(rand)
+  }
+
+  const handleVote = () => {
+    votes[selected] += 1;
+    const copy = [votes]
+    votes = copy
+    console.log(votes)
+  }
 
   return (
     <div>
       {anecdotes[selected]}
+      <br/><button onClick={handleVote}>vote</button><button onClick={handleNext}>next anecdote</button>
     </div>
   )
 }
